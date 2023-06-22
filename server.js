3828d163daba292e2ee1b7b718a1b4bfcb54fb67
monstergog <amn002@ucsd.edu>
2023-06-22 00:04:35 -0700

    notes can now be added and saved to the database

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
+
+app.use(express.json())
 
 app.use(express.static('public'));
 
@@ -19,7 +22,32 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
+    console.log(req.body);
 
+    const {title, text} = req.body;
+
+    const newNote = {
+        title,
+        text
+    }
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+
+            parsedNotes.push(newNote);
+
+            fs.writeFile('./db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr => {
+                writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully added new note!')
+            }))
+        }
+    });
 });
 
 app.get('*', (req, res) =>
